d9672ac39b4991d69dfe49c5ac6c06698fd8cbe2
chalik <47066050+fachalik@users.noreply.github.com>
2022-10-26 12:21:47 +0700

    feat: add config in vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import path from "path";
+import { defineConfig, loadEnv } from "vite";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-});
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: `${process.env.VITE_TENANT}.js`,
+        },
+      },
+    },
+  });
+};
