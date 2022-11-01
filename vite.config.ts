258649ef6b7a1428f2c8061fff705f1c473e25d1
chalik <47066050+fachalik@users.noreply.github.com>
2022-11-01 21:37:27 +0700

    fix: .env attribute

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig, loadEnv } from "vite";
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
@@ -15,7 +16,7 @@ export default ({ mode }) => {
     build: {
       rollupOptions: {
         output: {
-          entryFileNames: `${process.env.VITE_TENANT}.js`,
+          entryFileNames: `${process.env.TENANT}.js`,
         },
       },
     },
